f2ec05c9919b10d4da3f8287426d081ef6eb3d89
Belén Nazareth Durán <belennazareth29@gmail.com>
2023-06-04 23:26:47 +0200

    t2

--- tarea1/script.sh
+++ tarea1/script.sh
@@ -1,5 +1,10 @@
 #! /bin/sh
 
+while ! mysqladmin ping -h"$host_database" --silent; do
+    echo "Esperando a que el servicio de mysql este disponible..."
+    sleep 1
+done
+
 mysql -u $bookmedik_user --password=$bookmedik_passwd -h $host_database $db_name < /var/www/html/schema.sql
 
 /usr/sbin/apache2ctl -D FOREGROUND
